e7a9470d6a03c31b72b78c91bf9366e2363c88fe
Almanac <ruoyw0@gmail.com>
2021-04-23 22:20:46 -0700

    Started to playing around with passing property down a level.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
+import BookViewer from './BookViewer/bookViewer';
 
 class App extends Component {
-    state = { }
+    constructor(props) {
+        super(props);
+        this.books = [
+            {title: 'Ready Player One', author: 'Earnest Cline'},
+            {title:'All the Light We Cannot See', author: 'Anthony Doerr'},
+            {title: 'The First and Last Freedom', author: 'Jiddu Krishamurti'}
+        ];
+        this.state = {
+            bookNumber: 0
+        }
+    }
+    goToPreviousBook() {
+        let current = this.state.bookNumber;
+        let tempBookNumber =  current === 0 ? this.books.length -1 : current -1;
+        this.setState({
+            bookNumber: tempBookNumber
+        })
+    }
+    goToNextBook() {
+        let tempBookNumber = (this.state.bookNumber + 1) % this.books.length;
+        this.setState({
+            bookNumber: tempBookNumber
+        })
+    }
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <BookViewer book={this.books[this.state.bookNumber]} nextBook={()=> this.goToNextBook()} previousBook={()=> this.goToPreviousBook()} />
             </div>
         );
     }
